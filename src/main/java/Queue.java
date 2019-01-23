2e0ac1e8b04a0f05d51209e40db6870a166f321f
Seweryn91 <s.talaj@gmail.com>
2019-01-23 17:08:24 +0100

    Initial commit

---
+++ src/main/java/Queue.java
@@ -0,0 +1,23 @@
+public class Queue {
+
+    class Node {
+        private String value;
+        private Node nextNode;
+
+        public Node(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+        public Node getNextNode() {
+            return this.nextNode;
+        }
+
+        public void setNextNode(Node node) {
+            this.nextNode = node;
+        }
+    }
+}
