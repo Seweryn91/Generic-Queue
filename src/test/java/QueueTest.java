af69712f6d9e715d15542e9ea2185f1bd5045f7d
Seweryn91 <s.talaj@gmail.com>
2019-01-28 15:31:15 +0100

    Implement method enqueueTest_withPriority

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -1,5 +1,42 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
+
+    @Test
+    @DisplayName("Test enqueue without priority")
+    void enqueueTest() {
+        int expectedSize = 2;
+        Queue queue = createQueue(expectedSize);
+        assertEquals(expectedSize, queue.size());
+    }
+
+    @Test
+    @DisplayName("Test enqueue with priority")
+    void enqueueTest_withPriority() {
+        Queue<String> queue = new Queue<>();
+        queue.enqueue("Johann", 42);
+        queue.enqueue("Bach", 1);
+        queue.enqueue("Sebastian", 10);
+
+        String expectedResult = "Johann";
+        String actualResult = queue.peek();
+        assertEquals(expectedResult, actualResult);
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Test
+    private Queue createQueue(int size) {
+        Queue queue = new Queue();
+
+        for (int i = 0; i < size; i++) {
+            queue.enqueue(i);
+        }
+
+        return queue;
+    }
 
 }
